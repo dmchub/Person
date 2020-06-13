f53508e16c457a61812ac956d71ec33edf0d95ca
dmchub <37676729+dmchub@users.noreply.github.com>
2020-06-13 21:38:45 +0300

    Added tests for persons

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        Person woman1 = new Person(false, "Marry");
+        Person man1 = new Person(true, "Sam");
+        Person woman2 = new Person(false, "Donna");
+        Person man2 = new Person(true, "Garry");
+        Person woman3 = new Person(false, "Susy");
+
+        woman1.marry(man1);
+        System.out.println(woman1.toString() + " " + man1.toString());
+        woman1.marry(man2);
+        System.out.println(woman1.toString()+" "+" "+man2.toString());
+        woman1.marry(man2);
+        System.out.println(woman1.toString()+" "+" "+man2.toString());
+        woman1.marry(woman2);
+        System.out.println(woman1.toString()+" "+" "+woman2.toString());
+        woman2.marry(woman3);
+        System.out.println(woman2.toString()+" "+" "+woman3.toString());
+        man2.marry(woman3);
+        System.out.println(man2.toString()+" "+" "+woman3.toString());
+        System.out.println(man1.toString());
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,52 +1,44 @@
-import java.util.*;
-
 public class Person {
     private final boolean man;
     private final String name;
     private Person spouse;
-
-    List ls = new ArrayList();
 
     public Person(boolean man, String name) {
         this.man = man;
         this.name = name;
     }
 
-    /**
-     * This method checks gender of persons. If genders are not equal - tries to marry.
-     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife.
-     * Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
-     * @spouse person - new husband/wife for this person.
-     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
-     */
-
     public boolean marry(Person person){
         if(this.man != person.man){
-            if(this.spouse != null)
+            if(this.spouse != null){
                 this.divorce();
+            }
             if(person.spouse != null){
                 person.divorce();
             }
-            this.spouse = person;
-            return true;
+            if(this.spouse==null | person.spouse==null){
+                this.spouse = person;
+                person.spouse = this;
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
-    /**
-     * Sets spouse = null if spouse is not null
-     * @return true - if person status has been changed
-    */
-    private boolean divorce() {
+    public String toString(){
+        String spouseName = "Not marred";
+        if(this.spouse != null){
+            spouseName = this.spouse.name;
+        }
+        return this.name + " " + spouseName;
+    }
+
+    public boolean divorce() {
         if(this.spouse != null){
             this.spouse.spouse = null;
             this.spouse = null;
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
